87aab3b4a169ad31b867eeafa84dac3a46d389db
Alexandru Marian <Alexandru.Marian@centric.eu>
2023-01-13 15:07:49 +0200

    Fix grab enemy hand after breaking it
    Hand positions issue still wip

--- Assets/AutoHand/Scripts/Internal/GrabbableBase.cs
+++ Assets/AutoHand/Scripts/Internal/GrabbableBase.cs
@@ -8,9 +8,11 @@ using NaughtyAttributes;
 using UnityEditor;
 using UnityEngine.Serialization;
 
-namespace Autohand {
+namespace Autohand
+{
     [DefaultExecutionOrder(-2)]
-    public class GrabbableBase : MonoBehaviour{
+    public class GrabbableBase : MonoBehaviour
+    {
 
         [AutoHeader("Grabbable")]
         public bool ignoreMe;
@@ -52,16 +54,20 @@ namespace Autohand {
 
 
         private CollisionTracker _collisionTracker;
-        public CollisionTracker collisionTracker {
-            get {
-                if(_collisionTracker == null) {
+        public CollisionTracker collisionTracker
+        {
+            get
+            {
+                if (_collisionTracker == null)
+                {
                     _collisionTracker = gameObject.AddComponent<CollisionTracker>();
                     _collisionTracker.disableTriggersTracking = true;
                 }
                 return _collisionTracker;
             }
-            protected set {
-                if(_collisionTracker != null)
+            protected set
+            {
+                if (_collisionTracker != null)
                     Destroy(_collisionTracker);
 
                 _collisionTracker = value;
@@ -70,33 +76,36 @@ namespace Autohand {
 
 #if UNITY_EDITOR
         bool editorSelected = false;
-    #endif
+#endif
 
-        protected virtual void Awake() {
+        protected virtual void Awake()
+        {
             //Delete these layer setters if you want to use your own custom layer set
-            if(gameObject.layer == LayerMask.NameToLayer("Default") || LayerMask.LayerToName(gameObject.layer) == "")
+            if (gameObject.layer == LayerMask.NameToLayer("Default") || LayerMask.LayerToName(gameObject.layer) == "")
                 gameObject.layer = LayerMask.NameToLayer(Hand.grabbableLayerNameDefault);
-            
-            if(heldBy == null)
+
+            if (heldBy == null)
                 heldBy = new List<Hand>();
 
-            if(body == null){
-                if(GetComponent<Rigidbody>())
+            if (body == null)
+            {
+                if (GetComponent<Rigidbody>())
                     body = GetComponent<Rigidbody>();
                 else
                     Debug.LogError("RIGIDBODY MISSING FROM GRABBABLE: " + transform.name + " \nPlease add/attach a rigidbody", this);
             }
 
 
-    #if UNITY_EDITOR
-            if (Selection.activeGameObject == gameObject){
+#if UNITY_EDITOR
+            if (Selection.activeGameObject == gameObject)
+            {
                 Selection.activeGameObject = null;
                 Debug.Log("Auto Hand: Selecting the grabbable can cause lag and quality reduction at runtime. (Automatically deselecting at runtime) Remove this code at any time.", this);
                 editorSelected = true;
             }
 
             Application.quitting += () => { if (editorSelected) Selection.activeGameObject = gameObject; };
-    #endif
+#endif
 
             originalLayer = gameObject.layer;
             originalParent = body.transform.parent;
@@ -105,84 +114,101 @@ namespace Autohand {
             SetCollidersRecursive(body.transform);
         }
 
-        protected virtual void Start() {
-            if(!gameObject.CanGetComponent(out poseCombiner))
+        protected virtual void Start()
+        {
+            if (!gameObject.CanGetComponent(out poseCombiner))
                 poseCombiner = gameObject.AddComponent<GrabbablePoseCombiner>();
 
             GetPoseSaves(transform);
 
-            void GetPoseSaves(Transform obj) {
+            void GetPoseSaves(Transform obj)
+            {
                 //Stop if you get to another grabbable
-                if(obj.CanGetComponent(out Grabbable grab) && grab != this)
+                if (obj.CanGetComponent(out Grabbable grab) && grab != this)
                     return;
 
                 var poses = obj.GetComponents<GrabbablePose>();
-                for(int i = 0; i < poses.Length; i++)
+                for (int i = 0; i < poses.Length; i++)
                     poseCombiner.AddPose(poses[i]);
 
-                for(int i = 0; i < obj.childCount; i++)
+                for (int i = 0; i < obj.childCount; i++)
                     GetPoseSaves(obj.GetChild(i));
             }
         }
 
-        protected virtual void FixedUpdate() {
-            if(heldBy.Count > 0) {
+        protected virtual void FixedUpdate()
+        {
+            if (heldBy.Count > 0)
+            {
                 lastCenterOfMassRot = body.transform.rotation;
                 lastCenterOfMassPos = body.transform.position;
             }
         }
 
 
-        protected virtual void OnDisable(){
-            if (resetLayerRoutine != null){
+        protected virtual void OnDisable()
+        {
+            if (resetLayerRoutine != null)
+            {
                 StopCoroutine(resetLayerRoutine);
                 resetLayerRoutine = null;
             }
         }
-        
 
-        
-        internal void SetPlacePoint(PlacePoint point) {
+
+
+        internal void SetPlacePoint(PlacePoint point)
+        {
             this.placePoint = point;
         }
 
-        internal void SetGrabbableChild(GrabbableChild child) {
-            if(!grabChildren.Contains(child))
+        internal void SetGrabbableChild(GrabbableChild child)
+        {
+            if (!grabChildren.Contains(child))
                 grabChildren.Add(child);
         }
-        
 
 
 
 
-        protected int GetOriginalLayer(){
+
+        protected int GetOriginalLayer()
+        {
             return originalLayer;
         }
 
-        
-        internal void SetLayerRecursive(Transform obj, int oldLayer, int newLayer) {
-            for(int i = 0; i < grabChildren.Count; i++) {
-                if(grabChildren[i].gameObject.layer == oldLayer)
+
+        internal void SetLayerRecursive(Transform obj, int oldLayer, int newLayer)
+        {
+            for (int i = 0; i < grabChildren.Count; i++)
+            {
+                if (grabChildren[i] != null && grabChildren[i].gameObject.layer == oldLayer)
+                {
                     grabChildren[i].gameObject.layer = newLayer;
+                }
             }
             SetChildrenLayers(obj);
 
-            void SetChildrenLayers(Transform obj1){
-                if(obj1.gameObject.layer == oldLayer)
+            void SetChildrenLayers(Transform obj1)
+            {
+                if (obj1.gameObject.layer == oldLayer)
                     obj1.gameObject.layer = newLayer;
-                for(int i = 0; i < obj1.childCount; i++) {
+                for (int i = 0; i < obj1.childCount; i++)
+                {
                     SetChildrenLayers(obj1.GetChild(i));
                 }
             }
         }
 
-        internal void SetLayerRecursive(Transform obj, int newLayer) {
+        internal void SetLayerRecursive(Transform obj, int newLayer)
+        {
             SetLayerRecursive(obj, obj.gameObject.layer, newLayer);
         }
 
 
         //Invoked a quatersecond after releasing
-        protected IEnumerator IgnoreHandCollision(float time, Hand hand) {
+        protected IEnumerator IgnoreHandCollision(float time, Hand hand)
+        {
             IgnoreHand(hand, true);
 
             yield return new WaitForSeconds(time);
@@ -193,18 +219,22 @@ namespace Autohand {
 
         }
 
-        public bool GetSavedPose(out GrabbablePoseCombiner pose) {
-            if(poseCombiner.PoseCount() > 0) {
+        public bool GetSavedPose(out GrabbablePoseCombiner pose)
+        {
+            if (poseCombiner.PoseCount() > 0)
+            {
                 pose = poseCombiner;
                 return true;
             }
-            else {
+            else
+            {
                 pose = null;
                 return false;
             }
         }
 
-        public bool HasCustomPose() {
+        public bool HasCustomPose()
+        {
             return poseCombiner.PoseCount() > 0;
         }
 
@@ -212,29 +242,38 @@ namespace Autohand {
         public void IgnoreHand(Hand hand, bool ignore)
         {
             foreach (var col in grabColliders)
-                hand.HandIgnoreCollider(col, ignore);
+            {
+                if (col != null)
+                {
+                    hand.HandIgnoreCollider(col, ignore);
+                }
+            }
         }
 
         protected List<Collider> grabColliders = new List<Collider>();
-        void SetCollidersRecursive(Transform obj){
+        void SetCollidersRecursive(Transform obj)
+        {
             foreach (var col in obj.GetComponents<Collider>())
                 grabColliders.Add(col);
 
             for (int i = 0; i < obj.childCount; i++)
                 SetCollidersRecursive(obj.GetChild(i));
         }
-        
+
         //Resets to original collision dection
-        protected void ResetRigidbody() {
+        protected void ResetRigidbody()
+        {
             body.collisionDetectionMode = detectionMode;
             body.interpolation = startInterpolation;
         }
 
-        public bool BeingDestroyed() {
+        public bool BeingDestroyed()
+        {
             return beingDestroyed;
         }
 
-        public void DebugBreak() {
+        public void DebugBreak()
+        {
 #if UNITY_EDITOR
             Debug.Break();
 #endif
